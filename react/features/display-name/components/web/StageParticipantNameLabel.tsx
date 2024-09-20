8903fb5fe2af270cf33146d9f78a60bba51920bf
hbibanivideotranslator <hbibani@videotranslatorai.com.au>
2024-09-20 11:36:52 +1000

    merged changes correctly for stage participant name label

--- react/features/display-name/components/web/StageParticipantNameLabel.tsx
+++ react/features/display-name/components/web/StageParticipantNameLabel.tsx
@@ -1,29 +1,28 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { makeStyles } from "tss-react/mui";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { makeStyles } from 'tss-react/mui';
 
 import { IReduxState } from '../../../app/types';
-import { isDisplayNameVisible } from '../../../base/config/functions.any';
-import {
-    getLocalParticipant,
-    getParticipantDisplayName,
-    isWhiteboardParticipant
-} from '../../../base/participants/functions';
+import { getParticipantDisplayName, isScreenShareParticipant } from '../../../base/participants/functions';
 import { withPixelLineHeight } from '../../../base/styles/functions.web';
 import { getVideospaceFloatingElementsBottomSpacing } from '../../../base/ui/functions.web';
 import { getLargeVideoParticipant } from '../../../large-video/functions';
-import { getTransitionParamsForElementsAboveToolbox, isToolboxVisible } from '../../../toolbox/functions.web';
-import { isLayoutTileView } from '../../../video-layout/functions.web';
+import {
+    getTransitionParamsForElementsAboveToolbox,
+    isToolboxVisible,
+    toCSSTransitionValue
+} from '../../../toolbox/functions.web';
+import { isLayoutTileView } from '../../../video-layout/functions.any';
+import { createDisplayNameAndDialect }  from '../../../videotranslatorai/services/displayNameAndDialectService';
+import { shouldDisplayStageParticipantBadge } from '../../functions';
 
-import DisplayNameBadge from "./DisplayNameBadge";
+import DisplayNameBadge from './DisplayNameBadge';
 import {
     getStageParticipantFontSizeRange,
     getStageParticipantNameLabelLineHeight,
     getStageParticipantTypography,
-    scaleFontProperty,
-} from "./styles";
-
-import { createDisplayNameAndDialect }  from '../../../videotranslatorai/services/displayNameAndDialectService';
+    scaleFontProperty
+} from './styles';
 
 interface IOptions {
     clientHeight?: number;
@@ -31,11 +30,11 @@ interface IOptions {
 
 const useStyles = makeStyles<IOptions, 'screenSharing'>()((theme, options: IOptions = {}, classes) => {
     const typography = {
-        ...getStageParticipantTypography(theme),
+        ...getStageParticipantTypography(theme)
     };
     const { clientHeight } = options;
 
-    if (typeof clientHeight === "number" && clientHeight > 0) {
+    if (typeof clientHeight === 'number' && clientHeight > 0) {
         // We want to show the fontSize and lineHeight configured in theme on a screen with height 1080px. In this case
         // the clientHeight will be 960px if there are some titlebars, toolbars, addressbars, etc visible.For any other
         // screen size we will decrease/increase the font size based on the screen size.
@@ -56,21 +55,29 @@ const useStyles = makeStyles<IOptions, 'screenSharing'>()((theme, options: IOpti
     return {
         badgeContainer: {
             ...withPixelLineHeight(typography),
-            alignItems: "center",
-            display: "inline-flex",
-            justifyContent: "center",
+            alignItems: 'center',
+            display: 'inline-flex',
+            justifyContent: 'center',
             marginBottom: getVideospaceFloatingElementsBottomSpacing(theme, false),
-            transition: `margin-bottom ${getTransitionParamsForElementsAboveToolbox(false)}`,
+            transition: moveDownTransition,
             pointerEvents: 'none',
             position: 'absolute',
             bottom: 0,
             left: 0,
-            width: "100%",
-            zIndex: 1,
+            width: '100%',
+            zIndex: 1
         },
         containerElevated: {
             marginBottom: getVideospaceFloatingElementsBottomSpacing(theme, true),
-            transition: `margin-bottom ${getTransitionParamsForElementsAboveToolbox(true)}`
+            transition: moveUpTransition,
+            [`&.${classes.screenSharing}`]: {
+                opacity: 1,
+                transition: `${showTransition}, ${moveUpTransition}`
+            }
+        },
+        screenSharing: {
+            opacity: 0,
+            transition: `${hideTransition}, ${moveDownTransition}`
         }
     };
 });
@@ -81,11 +88,12 @@ const useStyles = makeStyles<IOptions, 'screenSharing'>()((theme, options: IOpti
  * @returns {ReactElement|null}
  */
 const StageParticipantNameLabel = () => {
-    const clientHeight = useSelector((state: IReduxState) => state["features/base/responsive-ui"].clientHeight);
+    const clientHeight = useSelector((state: IReduxState) => state['features/base/responsive-ui'].clientHeight);
     const { classes, cx } = useStyles({ clientHeight });
     const largeVideoParticipant = useSelector(getLargeVideoParticipant);
     const selectedId = largeVideoParticipant?.id;
-    //const nameToDisplay = useSelector((state: IReduxState) => getParticipantDisplayName(state, selectedId ?? '')); //videotranslatorai
+    
+    //const nameToDisplay = useSelector((state: IReduxState) => getParticipantDisplayName(state, selectedId ?? ''));
 
     //videotranslatorai
     const nameOfParticipant = useSelector((state: IReduxState) => getParticipantDisplayName(state, selectedId ?? ''));
@@ -98,28 +106,23 @@ const StageParticipantNameLabel = () => {
     //videotranslatorai
 
 
-    const localParticipant = useSelector(getLocalParticipant);
-    const localId = localParticipant?.id;
-
-    const isTileView = useSelector(isLayoutTileView);
     const toolboxVisible: boolean = useSelector(isToolboxVisible);
-    const showDisplayName = useSelector(isDisplayNameVisible);
+    const visible = useSelector(shouldDisplayStageParticipantBadge);
+    const isTileView = useSelector(isLayoutTileView);
+    const _isScreenShareParticipant = isScreenShareParticipant(largeVideoParticipant);
 
-    if (showDisplayName
-        && nameToDisplay
-        && selectedId !== localId
-        && !isTileView
-        && !isWhiteboardParticipant(largeVideoParticipant)
-    ) {
+    if (visible || (_isScreenShareParticipant && !isTileView)) {
+        // For stage participant visibility is true only when the toolbar is visible but we need to keep the element
+        // in the DOM in order to make it disappear with an animation.
         return (
             <div
                 className = { cx(
                     classes.badgeContainer,
-                    toolboxVisible && classes.containerElevated
+                    toolboxVisible && classes.containerElevated,
+                    _isScreenShareParticipant && classes.screenSharing
                 ) }>
                 <DisplayNameBadge name = { nameToDisplay } />
                 <DisplayNameBadge name = { dialectToDisplay } />
-
             </div>
         );
     }
