b02d57539c9718e72d491c9c6878c1d767a2e553
hbibanivideotranslator <hbibani@videotranslatorai.com.au>
2024-10-10 10:53:46 +1100

    feature/inperson complete

---
+++ react/features/videotranslatorai/components/buttons/inPersonToggleButton.tsx
@@ -0,0 +1,58 @@
+import React, { FC } from "react";
+
+import Tooltip from "../../../base/tooltip/components/Tooltip";
+import "./transcriptionButton.css"; // Import your CSS file
+
+interface InPersonButtonProps {
+    handleStart: () => void;
+    handleStop: () => void;
+    isRecording: boolean;
+    number: number;
+}
+
+const InPersonToggleButton: FC<InPersonButtonProps> = ({ isRecording, handleStart, handleStop, number }) => (
+    <Tooltip containerClassName="transcription-tooltip" content="Transcription/Translation" position="top">
+        <div className="toolbox-icon">
+            <div
+                className="circle-region"
+                onClick={isRecording ? handleStop : handleStart}
+                style={{
+                    backgroundColor: isRecording ? "green" : "transparent",
+                    cursor: "pointer",
+                    transition: "transform 0.3s ease",
+                    transform: isRecording ? "scale(1)" : "scale(0.9)",
+                    border: "2px solid white",
+                    borderRadius: "50%",
+                    width: "40px",
+                    height: "40px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    margin: "0 auto",
+                }}
+            >
+                <div className="jitsi-icon jitsi-icon-default" style={{ margin: "0 auto" }}>
+                    <div>
+                        <svg fill="none" height={20} viewBox="0 0 32 32" width={20} xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="16" cy="16" fill="none" r="15" stroke="white" strokeWidth="2" />
+                            <text
+                                dominantBaseline="middle"
+                                fill="white"
+                                fontFamily="Arial, sans-serif"
+                                fontSize="12"
+                                fontWeight="bold"
+                                textAnchor="middle"
+                                x="50%"
+                                y="50%"
+                            >
+                                {number}
+                            </text>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Tooltip>
+);
+
+export default InPersonToggleButton;
---
+++ react/features/videotranslatorai/services/inPersonServiceOpenAi.ts
@@ -0,0 +1,184 @@
+import axios from "axios";
+
+import { IReduxState } from "../../app/types";
+import { toState } from "../../base/redux/functions";
+
+import { createMessageStorageSendTranslationToDatabase } from "./messageService";
+
+export const inPersonServiceOpenAi = async (
+    dispatch: any,
+    getState: any,
+    recordedBlobParam: any,
+    langFrom: any,
+    langFromTranslation: any = "",
+    participantName: any = ""
+) => {
+    const state: IReduxState = getState();
+
+    const tokenData = toState(state)["features/videotranslatorai"].jwtToken;
+    const participantState = toState(state)["features/base/participants"];
+    const participantData = toState(state)["features/videotranslatorai"].participantData;
+    const meetingData: any = toState(state)["features/videotranslatorai"].meetingData;
+    const moderatorData = toState(state)["features/videotranslatorai"].moderatorData;
+    const entityData: any = toState(state)["features/videotranslatorai"].thisEntityData;
+    const conference = toState(state)["features/base/conference"].conference;
+    const clientId = toState(state)["features/videotranslatorai"].clientId;
+    const meetingId = toState(state)["features/videotranslatorai"].meetingId;
+    const participantAndModeratorData = [...moderatorData, ...participantData];
+
+    const translateApiKey = process.env.REACT_APP_MICROSOFT_TRANSLATE_API_KEY;
+    const transcriptionEndpoint = process.env.REACT_APP_OPENAI_TRANSCRIPTION_ENDPOINT;
+    const translationEndpoint = process.env.REACT_APP_MICROSOFT_TRANSLATION_ENDPOINT;
+    const apiEndpoint = process.env.REACT_APP_OPENAI_TRANSCRIBE_ENDPOINT_VIDEOTRANSLATORAI; // New API endpoint
+
+    if (!translateApiKey || !transcriptionEndpoint || !translationEndpoint || !apiEndpoint) {
+        throw new Error("One or more environment variables are not set.");
+    }
+
+    try {
+        // langFrom = participant.transcriptionDialect.dialectCode;
+        // langFromTranslation = participant.translationDialect.dialectCode;
+
+        const formData = new FormData();
+
+        // You can get the current timestamp or provide a custom lastModified date
+        const lastModifiedDate = new Date(); // Set it to the current date and time, or any desired date
+        const lastModified = lastModifiedDate.getTime(); // Get the timestamp in milliseconds
+
+        // Create a complete File object with custom lastModified date and type
+        const correctedBlob = new File([recordedBlobParam.blob], "audio.wav", {
+            type: "audio/wav",
+            lastModified, // Assign the custom lastModified timestamp
+        });
+
+        formData.append("file", correctedBlob); // Append the corrected Blob object, ensure filename
+        // formData.append("file", correctedBlob); // Append the corrected Blob object
+        formData.append("langFrom", langFrom); // Specify the language (optional)
+
+        // Set the new API endpoint for transcription
+
+        // Make the request to the new API
+        const transcriptionResponse = await axios.post(apiEndpoint, formData, {
+            headers: {
+                Authorization: `Bearer ${tokenData}`, // Bearer token for authentication
+                "Content-Type": "multipart/form-data", // Ensure the correct content type for form data
+            },
+        });
+
+        // Extract the transcription text from the response
+        const transcriptionText = transcriptionResponse.data.data.transcription;
+
+        console.log("TRANSCRIPTION TEXT", transcriptionText);
+        if (!transcriptionText) {
+            throw new Error("Transcription failed: No text returned.");
+        }
+
+        await Promise.all(
+            participantAndModeratorData.map(async (participant) => {
+                if (
+                    participant.translationDialect.dialectCode &&
+                    participant.transcriptionDialect.dialectCode !== langFrom &&
+                    conference
+                ) {
+                    try {
+                        // Translate the transcribed text
+                        const translationResponse = await axios.post(
+                            translationEndpoint,
+                            [{ Text: transcriptionText }],
+                            {
+                                headers: {
+                                    "Ocp-Apim-Subscription-Key": translateApiKey,
+                                    "Ocp-Apim-Subscription-Region": "australiaeast",
+                                    "Content-Type": "application/json",
+                                },
+                                params: {
+                                    // from: langFromTranslation,
+                                    to: participant.translationDialect.dialectCode,
+                                },
+                            }
+                        );
+
+                        const translationText = translationResponse.data[0].translations[0].text;
+                        const translationSent = `${participantName}: ${translationText} (videotranslatoraiservice)`;
+
+                        let participantId = "";
+
+                        if (conference) {
+                            participantId = conference.myUserId();
+                        }
+
+                        // arrayPromises.push(await conference.sendPrivateTextMessage(participantId, translationSent));
+                        await conference.sendPrivateTextMessage(participantId, translationSent);
+                        const messageData: any = {
+                            meeting_project_id: meetingId,
+                            client_id: clientId,
+                            dialect_from: entityData.transcriptionDialect.dialectId,
+                            dialect_to: participant.translationDialect.dialectId,
+                            original_text: transcriptionText,
+                            translated_text: translationText,
+                        };
+
+                        if (entityData.type === "MODERATOR") {
+                            messageData.moderator_id = entityData.moderatorId;
+                        } else if (entityData.type === "PARTICIPANT") {
+                            messageData.participant_id = entityData.participantId;
+                        }
+
+                        await createMessageStorageSendTranslationToDatabase(messageData, tokenData);
+
+                        // dispatch(
+                        //     addMessageVideoTranslatorAI({
+                        //         displayName: participantName,
+                        //         hasRead: true,
+                        //         participantId,
+                        //         messageType: "local",
+                        //         message: translationSent,
+                        //         privateMessage: true,
+                        //         timestamp: Date.now(),
+                        //         isReaction: false,
+                        //         recipient: participant.name,
+                        //         error: null,
+                        //         messageId: null,
+                        //         lobbyChat: null,
+                        //     })
+                        // );
+                    } catch (error) {
+                        console.error(`Error during translation for participant ${participant.name}:`, error);
+                    }
+                }
+            })
+        );
+
+        // TODO: this is not the viable solution to send two at the same time, it's a temporary fix
+        // TODO: try to work out another way of solving this problem
+        // TODO: the issue is that when a single message is being sent, it only sends on pressing the button a second time
+
+        await Promise.all(
+            participantAndModeratorData.map(async (participant) => {
+                if (
+                    participant.translationDialect.dialectCode &&
+                    participant.transcriptionDialect.dialectCode !== langFrom
+                ) {
+                    try {
+                        const translationSent = " (videotranslatoraiservice)";
+                        let participantId = "";
+
+                        if (conference) {
+                            participantId = conference.myUserId();
+                        }
+
+                        if (participantId && conference) {
+                            await conference.sendPrivateTextMessage(participantId, translationSent);
+                        }
+                    } catch (error) {
+                        console.error(`Error during translation for participant ${participant.name}:`, error);
+                    }
+                }
+            })
+        );
+
+        // Await all translation promises
+    } catch (err) {
+        console.error("Error during transcription and translation:", err);
+    }
+};
